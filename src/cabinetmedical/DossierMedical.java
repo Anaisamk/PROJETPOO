f16904917f6b1b8867294e9dc9b277a1cc2aafa4
anais <anais@LAPTOP-9HH0H0RO>
2024-04-24 16:18:09 +0100

    YYY

--- src/cabinetmedical/DossierMedical.java
+++ src/cabinetmedical/DossierMedical.java
@@ -11,16 +11,16 @@ package cabinetmedical;
 public class DossierMedical{
     private int num_dossier;
     private Patients patient;
-    private Historique_traitements traitements; //besoin de la classe
+     //besoin de la classe
     private String diagnostic;
     private Prescription prescription; //besoin de la classe prescription
 
 
     //constructeur
-    public DossierMedical(int num_dossier,Patients patient,Historique_traitements traitements,String diagnostic,Prescription prescription){
+    public DossierMedical(int num_dossier,Patients patient,String diagnostic,Prescription prescription){
         this.num_dossier = num_dossier;
         this.patient = patient;
-        this.traitements = traitements; // besoin de la classe historique_traitements
+        // besoin de la classe historique_traitements
         this.diagnostic = diagnostic;
         this.prescription = prescription; // besoin de la classe prescription
     }
@@ -32,9 +32,7 @@ public class DossierMedical{
     public Patients get_patient(){
         return patient;
     }
-    public Historique_traitements get_traitements(){
-        return traitements;
-    }
+
     public String get_diagnostic(){
         return diagnostic;
     }
@@ -49,9 +47,7 @@ public class DossierMedical{
     public void set_patient(Patients patient) {
         this.patient = patient;
     }
-    public void set_traitements(Historique_traitements traitements) {
-        this.traitements = traitements;
-    }
+
     public void set_diagnostic(String diagnostic) {
         this.diagnostic = diagnostic;
     }
@@ -61,16 +57,14 @@ public class DossierMedical{
     }
 
     //ajouter un traitement
-    public void ajout_traitement(){
-        traitements.ajouter_traitement();  // methode de la classe historique_traitements
-    }
+
 
     //affichage du dossier medical
     public void afficher_dossier_medical(){
         patient.affichage_patient();
-        traitements.affichage_traitement();  //on a besoin de la classe
+       //on a besoin de la classe
         System.out.println("diagnostic:"+diagnostic);
-        prescription.affichge_prescription(); //besoin de la classe
+         //besoin de la classe
     }
 
 
